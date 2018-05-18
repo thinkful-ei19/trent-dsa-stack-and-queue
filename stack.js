1b65921dd34ba272bae5e81de03da6ae07334d0b
trentduncan <trent.dunc@gmail.com>
2018-05-18 13:07:45 -0700

    finished stack portion

--- stack.js
+++ stack.js
@@ -13,11 +13,18 @@ class Stack {
   }
 
   push(data) {
+    //if the top of the stack is empty, then the data will be the
+    //top of the stack
     if (this.top === null) {
       this.top = new _Node(data, null);
-      return;
+      return this.top;
     }
-    this.top = new _Node(data, this.top);
+
+    //if the top already has something then create a new node
+    //add data to the new node
+    // have the pointer point to the top 
+    const node = new _Node(data, this.top);
+    this.top = node;
   }
 
   pop() {
@@ -37,7 +44,14 @@ function main() {
   //   starTrek.pop();
 
   //   peek(starTrek);
-  display(starTrek);
+  //   display(starTrek);
+  const stack = new Stack();
+  stack.push(5);
+  stack.push(4);
+  stack.push(6);
+  //   stack.pop();
+//   sortStack(stack);
+//   display(stack);
 
   
 
@@ -73,4 +87,100 @@ function is_palindrome(s) {
   return true; 
 }
 
-console.log(is_palindrome('dad'));
+// O(n)
+// console.log(is_palindrome('dad'));
+
+function matching(str) {
+  const openStack = new Stack();
+  const closedStack = new Stack();
+  let openParens = 0;
+  let closedParens = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(') {
+      openStack.push(i);
+      openParens++;
+    }
+    else if (str[i] === ')') {
+      closedStack.push(i);
+      closedParens++;
+    }
+  }
+  if (openParens === closedParens) {
+    return true;
+  }
+  else if (openParens > closedParens) {
+    const position = openStack.pop();
+    return `Error - open paren at: ${position}`;
+  }
+  else if (openParens < closedParens) {
+    const position = closedStack.pop();
+    return `Error - closed paren at: ${position}`;
+  }
+}
+
+// function matching(str) {
+//   const parensStack = new Stack();
+//   let isOpenParen = true;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === '(') {
+//       parensStack.push(i);
+//     }
+//     else if (str[i] === ')') {
+//       parensStack.pop();
+//     }
+//   }
+//   console.log(parensStack);
+//   if (parensStack.top === null) {
+//     return true;
+//   }
+//   else if (isOpenParen) {
+//     for () {
+
+//     }
+//   }
+// }
+
+// console.log(matching('(5 + (4 - 2'));
+
+// function sortStack(stack) {
+//   let topLevelNode = stack.top; 
+//   let lowerlevelNode = stack.top.next;
+//   let resultStack = new Stack();
+//   let currentGreatest = stack.pop();
+//   while (topLevelNode !== null) {
+//     // console.log(topLevelNode);
+//     while (lowerlevelNode !== null) {
+//       let currentEval = stack.pop();
+//       console.log(currentEval);
+//       if (currentEval > currentGreatest) {
+//         currentGreatest = currentEval;
+//       }
+//       lowerlevelNode = lowerlevelNode.next;
+//     //   console.log(lowerlevelNode);
+//     }
+//     resultStack.push(currentGreatest);
+//     topLevelNode = topLevelNode.next;
+//   }
+//   return resultStack;
+// }
+
+function sortStack(stack){
+  let holder = new Stack();
+  while (stack.top) {
+    let num = stack.pop();
+    while(holder.top && peek(holder) > num) {
+      stack.push(holder.pop());
+    }
+    holder.push(num);
+  }
+  // while loop through original 
+  //compare while loop 
+  // pushing into stack
+  return holder;
+}
+let stack = new Stack();
+stack.push(1);
+stack.push(9);
+stack.push(6);
+console.log(JSON.stringify(sortStack(stack)));
+
